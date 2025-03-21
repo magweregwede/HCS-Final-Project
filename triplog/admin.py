99832250023c8b9e7ac1f06b73c2045b189db03c
magweregwede <magweregwede10@gmail.com>
2025-03-21 23:16:08 +0200

    Add fields to model pages in Admin
    Enable sorting through data by various fields.

--- triplog/admin.py
+++ triplog/admin.py
@@ -1,11 +1,38 @@
 from django.contrib import admin
 from .models import Driver, Product, Route, Trip, TripProduct, TripRoute, Truck, TruckCompany
 
+class DriverAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "assigned_truck",
+        "contact",
+    ]
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "category",
+    ]
+
+class RouteAdmin(admin.ModelAdmin):
+    list_display = [
+        "origin",
+        "destination",
+        "distance_km",
+    ]
+
+class TruckAdmin(admin.ModelAdmin):
+    list_display = [
+        "plate_number",
+        "truck_company",
+        "capacity_kg"
+    ]
+
 # Register your models here.
-admin.site.register(Driver)
-admin.site.register(Product)
-admin.site.register(Route)
-admin.site.register(Truck)
+admin.site.register(Driver, DriverAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Route, RouteAdmin)
+admin.site.register(Truck, TruckAdmin)
 admin.site.register(TruckCompany)
 admin.site.register(Trip)
 admin.site.register(TripProduct)
--- triplog/models.py
+++ triplog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class TruckCompany(models.Model):
     name = models.CharField(max_length=255)
     contact = models.CharField(max_length=20)
-    email = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
     address = models.CharField(max_length=255)
 
     def __str__(self):
@@ -67,7 +67,7 @@ class TripProduct(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="trip_products")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    unit = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50, choices=[("Pallets", "Pallets"), ("Cases", "Cases")])
 
     def __str__(self):
         return f"{self.product.name} ({self.quantity} {self.unit}) for Trip {self.trip.id}"
