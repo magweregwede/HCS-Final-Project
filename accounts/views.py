37cd7b6f9c5e7e04d61293a02efc4bd44adaaef0
magweregwede <magweregwede10@gmail.com>
2025-03-23 00:50:53 +0200

    Add pages app for static pages

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,5 @@ from django.urls import reverse_lazy
 
 # Create your views here.
 
+# no views since this app is for log in and log out,
+# which are django built-in functions.
--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "triplog",
     "accounts",
+    "pages",
 ]
 
 MIDDLEWARE = [
